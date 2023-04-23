a4500bee1e1f5ed100d6adbe0490369d6a656d2b
katatoropcova <127528957+katatoropcova@users.noreply.github.com>
2023-04-23 11:28:02 +0300

    Add files via upload

--- js/gameMenu.js
+++ js/gameMenu.js
@@ -1,4 +1,29 @@
-export const createGameMenu = () => {
+import { startGame } from "./startGame.js"
+
+
+export const createGameMenu = (flag) => {
+	const btnPrev = document.querySelector('.header__btn-prev');
+	btnPrev.style.display = "none";
+
+	const score = document.querySelector('.score');
+	console.log(score.textContent);
+	let s1 = Number(score.textContent);
+	score.style.display = "none";
+
+	const bestScore = document.querySelector('.best_score');
+	console.log(bestScore.textContent);
+	if (bestScore.textContent == "") {
+		bestScore.style.visibility = "hidden";
+		bestScore.textContent = "РЕКОРД: 1000000";
+	}
+	else {
+		if (Number(bestScore.textContent.slice(8, bestScore.textContent.length)) > s1 && flag) {
+			bestScore.textContent = `РЕКОРД: ${s1}`;
+			bestScore.style.visibility = "visible";
+		}
+	}
+
+
 	const title = document.createElement('h2');
 	title.classList.add('game-menu__title');
 	const gameSection = document.querySelector('.game-section__container');
@@ -14,7 +39,7 @@ export const createGameMenu = () => {
 		else if (lvl == 2) lvl = 'средний';
 		else if (lvl == 3) lvl = 'сложный';
 		button.textContent = lvl;
-		button.addEventListener('click', () => { });
+		button.addEventListener('click', () => startGame(lvl));
 		return button;
 	}
 
